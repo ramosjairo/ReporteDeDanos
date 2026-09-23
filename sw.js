3da6bdebccb09d31b954160d793bac275779fc22
Jairo Ramos <33663007+ramosjairo@users.noreply.github.com>
2026-09-23 06:39:19 -0400

    Add files via upload
    version Iniciar

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'inspeccion-app-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './modulo_formulario_3.html',
+  './inspeccion_menu.html',
+  './jszip.min.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        // Retorno sin conexión seguro
+      });
+    })
+  );
+});
